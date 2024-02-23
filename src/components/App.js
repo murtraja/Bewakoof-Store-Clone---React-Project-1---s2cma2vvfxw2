dfc62af70596e9d1579c7038cdc595767021f426
Murtaza Raja <murt.raja@gmail.com>
2024-02-23 23:22:53 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
 import "../styles/App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div>
+      <nav>
+        <div className="nav">
+          <div className="navLeft">
+            <img src="https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1637759004/Croma%20Assets/CMS/Category%20icon/Final%20icon/Croma_Logo_acrkvn.svg" />
+            <div>
+              <span>≡</span> Menu
+            </div>
+            <div className="navSearch">
+              <input type="text" placeholder="What are you looking for?" />
+              <div>🔍</div>
+            </div>
+          </div>
+          <div className="navRight">
+            <div className="navAddress">
+              <div>📌</div>
+              <div>New Delhi, 110009</div>
+              <div className="navAddressPencil">🖉</div>
+            </div>
+            <div>👤</div>
+            <div className="navCart">🛒</div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
