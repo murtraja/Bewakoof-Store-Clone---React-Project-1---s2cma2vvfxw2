9f087d21672362e0d1bce9ea1282ef41a1702e3c
Murtaza Raja <murt.raja@gmail.com>
2024-02-23 21:57:39 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,53 @@
 import "../styles/App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div>
+      <nav>
+        <div className="nav">
+          <div className="logoNav">
+            <img src="https://www.thesouledstore.com/static/img/300x157-twitter.png" />
+          </div>
+          <div className="topBottomNav">
+            <div className="topNav">
+              <div className="topNavLeft">
+                <span>WOMEN</span>
+                <span>MEN</span>
+                <span>KIDS</span>
+              </div>
+              <div className="topNavRight">
+                <span>TRACK ORDER</span>
+                <span>CONTACT US</span>
+                <span>DOWNLOAD APP</span>
+              </div>
+            </div>
+            <div className="bottomNav">
+              <div className="bottomNavLeft">
+                <div className="categoryParent">
+                  <span>TOPWEAR</span>
+                  {/* <div className="categoryUnderline" /> */}
+                </div>
+                <div className="categoryParent">
+                  <span>BOTTOMWEAR</span>
+                  {/* <div className="categoryUnderline" /> */}
+                </div>
+                <div className="categoryParent">
+                  <span>SNEAKERS</span>
+                  {/* <div className="categoryUnderline" /> */}
+                </div>
+              </div>
+              <div className="bottomNavRight">
+                <span>S</span>
+                <span>P</span>
+                <span>W</span>
+                <span>C</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
